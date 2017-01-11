7710e4f9ad8f3581803707b3e6f649acd715ac0a
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2017-01-11 10:30:15 +0000

    Module 35, edit and update comment

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -38,7 +38,6 @@ router.post("/", isLoggedIn, function(req, res){
                if(err){
                    console.log(err);
                }else{
-                   
                    //if comment saved successfully, add comment to campground
                    campground.comments.push(comment);
                    //save campground
@@ -59,6 +58,30 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //EDIT - show the edit form
+router.get("/:comment_id/edit", function(req, res){
+    
+    Comment.findById(req.params.comment_id, function(err, comment){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        }else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: comment});
+        }
+    });
+});
+
+//UPDATE - save the changed comment
+router.put("/:comment_id/", function(req, res){
+    
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment){
+        if(err){
+            console.log(err);
+            res.redirect("back")
+        }else{
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    });
+})
 
 /******************************************/
 
